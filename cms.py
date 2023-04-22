d7e7f12495c94e0a7d7fa3d10dfe8a3126e0a887
Johnny-kiv <johnny.kiv@yandex.ru>
2023-04-22 11:43:44 +0300

    m

--- cms.py
+++ cms.py
@@ -1,40 +1,68 @@
-from aiogram import Bot,Dispatcher,executor,types
-from aiogram.types import ReplyKeyboardMarkup,KeyboardButton
-import sqlite3
+from aiogram import Bot, Dispatcher, types,executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 insert = KeyboardButton("insert")
-select = KeyboardButton("select")
-mainM = ReplyKeyboardMarkup(resize_keyboard=True).add(insert,select)
-bot = Bot(token="6021724545:AAHY5UvQ2ttRGnazCJH2ZlVXI_oORAZbnIA")
-dp = Dispatcher(bot=bot)
-conn = sqlite3.connect("tovary.db")
-cur = conn.cursor()
-global flag
-flag = 2
-@dp.message_handler(commands=['start'])
-async def start(mes: types.Message):
-    await bot.send_message(mes.from_user.id,text=f"Здравствуйте {mes.from_user.first_name}! Добро пожаловать в компанию Барахолка. Мы предстовляем собой интернет магазмн. Здесь каждый может стать продавцом и продовая все что хочет (кроме оружия). Так что начните свой бизнес здесь! Безопасно удобно и выгодно. Удачи!",reply_markup=mainM)
-@dp.message_handler()
-async def handler1(mes: types.Message):
-    global name, flag
-    if mes.text == "insert":
-        if flag==2:
-            await bot.send_message(mes.from_user.id, "Введите название товара:", reply_markup=mainM)
-            flag = 0
-@dp.message_handler()
-async def handler2(mes: types.Message):
-    global price, flag, name
-    if flag==0:
-        await bot.send_message(mes.from_user.id, "Введите цену товара:", reply_markup=mainM)
-        name = mes.text
-        flag = 1
+cancel = KeyboardButton("cancel")
+mainM = ReplyKeyboardMarkup(resize_keyboard=True).add(insert)
+bot = Bot(token='6021724545:AAHY5UvQ2ttRGnazCJH2ZlVXI_oORAZbnIA')
 
+# Don't forget to use storage, otherwise answers won't be saved.
+# You can find all supported storages here:
+# https://github.com/aiogram/aiogram/tree/dev-2.x/aiogram/contrib/fsm_storage
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+
+class Form(StatesGroup):
+    name = State()
+    price = State()
+    image = State()
+
+
+@dp.message_handler(commands=['start'],state="*")
+async def start(mes: types.Message):
+    await bot.send_message(mes.from_user.id,text="HI",reply_markup=mainM)
 @dp.message_handler()
-async def handler3(mes: types.Message):
-    global price, flag, name
-    if flag==1:
-        price = mes.text
-        flag = 2
-    data = [name, price, None,None]
-    cur.execute("INSERT INTO Tovary VALUES(?, ?, ?,?);", data)
-    conn.commit()
-executor.start_polling(dp,skip_updates=True)
+async def handler(mes: types.Message):
+    """Conversation entrypoint"""
+    if mes.text == "insert":
+
+        # Set state
+        await Form.name.set()
+        await mes.reply("Введите имя товара:",reply_markup=mainM)
+
+# You can use state='*' if you want to handle all states
+@dp.message_handler(state='*', commands=['cancel'])
+async def cancel_handler(mes: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    await state.finish()
+    await mes.reply('Cancelled.',reply_markup=mainM)
+
+
+@dp.message_handler(state=Form.name)
+async def process_name(mes: types.Message, state: FSMContext):
+    await state.finish()
+    global name
+    name = mes.text
+    await Form.price.set()
+    await mes.reply("Введите цену товара:",reply_markup=mainM)  # <-- Here we get the name
+
+@dp.message_handler(state=Form.price)
+async def process_price(message: types.Message, state: FSMContext):
+    await state.finish()
+    global name,summa
+    summa = message.text
+    await message.reply("Пришлите изображение:",reply_markup=mainM)  # <-- Here we get the name
+
+@dp.message_handler(state=Form.image)
+async def process_image(message: types.Message, state: FSMContext):
+    await state.finish()
+    global name,summa,image
+    file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    await message.reply(f"Товар: {name}\n Сумма: {summa}",reply_markup=mainM)  # <-- Here we get the name
+executor.start_polling(dp,loop=True,skip_updates=True)
